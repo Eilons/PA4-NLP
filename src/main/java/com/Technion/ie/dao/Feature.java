e7db3396ad56e88fe177110db2dfadf7c7e4c7b5
Eilons <eilon16@gmail.com>
2015-01-14 20:14:16 +0200

    create feature class

--- src/main/java/com/Technion/ie/dao/Feature.java
+++ src/main/java/com/Technion/ie/dao/Feature.java
@@ -2,7 +2,10 @@ package com.Technion.ie.dao;
 
 import java.io.IOException;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
+
+import com.Technion.ie.Utils.Utils;
 //Not sure still if needed!!!!
 public class Feature extends Parameter {
 	
@@ -35,12 +38,22 @@ public class Feature extends Parameter {
             }
         }
         //TAG FEATURE
-        Set<String> words = new HashSet<String> ();//keep all seeing words - acts like cache
+        Set<String> words = new HashSet<String> ();//keep all seeing words:Tags - acts like cache
         //read from word files...
-        
-        
-        
-		
+		List<String> trainingData;
+		trainingData = Utils.readFile(path);
+		for (String data : trainingData) 
+		{
+			String[] string = Utils.splitToTokens(data);
+			if (string.length == 2)
+			{
+				String wordTagPair = string[0]+":"+string[1];
+				if (!words.contains(wordTagPair)) {
+						words.add(wordTagPair);
+						this.mapParameter.put(wordTagPair, 1.0);
+				}
+			}
+		}
 	}
 	
 
